bd6a58beff3aa1a675a9013f2116e8f5d77902b1
ChrisVonVolotor <evilchris9@gmail.com>
2018-06-21 16:49:17 +0100

    commit 15

--- New folder/com.practiceselenium/src/test/java/com/qa/quickstart/com/practiceselenium/TeaTestingSteps.java
+++ New folder/com.practiceselenium/src/test/java/com/qa/quickstart/com/practiceselenium/TeaTestingSteps.java
@@ -1,12 +1,11 @@
 package com.qa.quickstart.com.practiceselenium;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.relevantcodes.extentreports.ExtentReports;
+//import com.relevantcodes.extentreports.ExtentReports;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -14,41 +13,37 @@ import cucumber.api.java.en.When;
 
 public class TeaTestingSteps {
 	public ChromeDriver driver;
-	static ExtentReports extent;
-	
+	//static ExtentReports extent;
 	
 	@Given("^the correct web address$")
 	public void isCorrectAddress() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dale\\Desktop\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Admin\\\\Documents\\\\Development\\\\chromedriver_win32\\\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		assertEquals("http://www.practiceselenium.com/welcome.html", driver.getCurrentUrl());
+		driver.navigate().to("http://www.practiceselenium.com/welcome.html");
+
+		//assertEquals("http://www.practiceselenium.com/welcome.html", driver.getCurrentUrl());
 
 	}
+	
 	@When("^I navigate to the 'Menu' page$")
 	public void setNavMenu() {
 		driver.navigate().to("http://www.practiceselenium.com/menu.html");
 	}
 	
-	@Then("^I can browse a list of the available products$")
+	@Then("^I can browse a list of the available products\\.")
 	public void checkTeaIsHere() {
 		WebElement itemsForSale = driver.findElementByXPath("//*[@id=\"wsb-element-00000000-0000-0000-0000-000453230000\"]/div/p/span/span/strong");
+
 		assertEquals(itemsForSale.getText(), "Green Tea");
 		driver.close();
 		driver.quit();
 	}
 	
-	@Given("^the correct web address$")
-	public void setUpWebDriver() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dale\\Desktop\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		assertEquals("http://www.practiceselenium.com/menu.html", driver.getCurrentUrl());
-
-	}
+	
 	@When("^I click the checkout button$")
 	public void webDriverNavigate() {
-		WebElement checkOut = driver.findElementByXPath("//*[@id=\"wsb-button-00000000-0000-0000-0000-000451955160\"]/span");
+		
+		WebElement checkOut = driver.findElementByXPath("//*[@id=\"wsb-nav-00000000-0000-0000-0000-000450914915\"]/ul/li[5]/a");
 		checkOut.click();
 	}
 	
